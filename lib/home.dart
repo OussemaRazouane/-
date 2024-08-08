a1f2e9449aff7ab58cdd451b52da6f2016b2e448
Oussema Razouane <oussemarznpro@gmail.com>
2024-08-08 01:16:17 +0100

    Without State

---
+++ lib/home.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<String> tasbeh = [
+  "سُبْحَانَ اللَّهِ",
+  "سُبْحَانَ اللَّهِ وَبِحَمْدِهِ",
+  "سُبْحَانَ اللَّهِ وَالْحَمْدُ لِلَّهِ",
+  "سُبْحَانَ اللهِ العَظِيمِ وَبِحَمْدِهِ",
+  "سُبْحَانَ اللَّهِ وَبِحَمْدِهِ ، سُبْحَانَ اللَّهِ الْعَظِيمِ",
+  "لَا إلَه إلّا اللهُ وَحْدَهُ لَا شَرِيكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ وَهُوَ عَلَى كُلُّ شَيْءِ قَدِيرِ",
+  "لا حَوْلَ وَلا قُوَّةَ إِلا بِاللَّهِ ",
+  "الْحَمْدُ للّهِ رَبِّ الْعَالَمِينَ",
+  "الْلَّهُم صَلِّ وَسَلِم وَبَارِك عَلَى سَيِّدِنَا مُحَمَّد",
+  "أستغفر الله ",
+  "سُبْحَانَ الْلَّهِ، وَالْحَمْدُ لِلَّهِ، وَلَا إِلَهَ إِلَّا الْلَّهُ، وَالْلَّهُ أَكْبَرُ",
+  "لَا إِلَهَ إِلَّا اللَّهُ",
+  "الْلَّهُ أَكْبَرُ",
+  "سُبْحَانَ اللَّهِ ، وَالْحَمْدُ لِلَّهِ ، وَلا إِلَهَ إِلا اللَّهُ ، وَاللَّهُ أَكْبَرُ ، اللَّهُمَّ اغْفِرْ لِي ، اللَّهُمَّ ارْحَمْنِي ، اللَّهُمَّ ارْزُقْنِي.",
+  "الْحَمْدُ لِلَّهِ حَمْدًا كَثِيرًا طَيِّبًا مُبَارَكًا فِيهِ",
+  "اللَّهُ أَكْبَرُ كَبِيرًا ، وَالْحَمْدُ لِلَّهِ كَثِيرًا ، وَسُبْحَانَ اللَّهِ بُكْرَةً وَأَصِيلاً.",
+  "اللَّهُمَّ صَلِّ عَلَى مُحَمَّدٍ وَعَلَى آلِ مُحَمَّدٍ كَمَا صَلَّيْتَ عَلَى إِبْرَاهِيمَ , وَعَلَى آلِ إِبْرَاهِيمَ إِنَّكَ حَمِيدٌ مَجِيدٌ , اللَّهُمَّ بَارِكْ عَلَى مُحَمَّدٍ وَعَلَى آلِ مُحَمَّدٍ كَمَا بَارَكْتَ عَلَى إِبْرَاهِيمَ وَعَلَى آلِ إِبْرَاهِيمَ إِنَّكَ حَمِيدٌ مَجِيدٌ.",
+  ];
+  int index=0;
+  int reapt=0;
+  int round=0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(fit:BoxFit.cover ,image: NetworkImage("https://i.pinimg.com/736x/49/27/c8/4927c872ba6515d4fe840b1aae29d2bb.jpg")),
+        ),
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              width:MediaQuery.of(context).size.width/1.5 ,
+              height: MediaQuery.of(context).size.height/4,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color:const Color.fromARGB(167, 194, 170, 170),
+              ),
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed:(){
+                      setState(() {
+                        index--;
+                        if(index<0) index=tasbeh.length-1;
+                      });
+                    },
+                    icon: const Icon(Icons.arrow_back_ios_new,)
+                  ),
+                  Expanded(
+                    child: Text(
+                      tasbeh[index],
+                      textAlign:TextAlign.center,
+                      style:const TextStyle(fontSize: 25, fontWeight: FontWeight.bold,),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed:(){
+                      setState(() {
+                        index++;
+                        if(index>tasbeh.length-1) index=0;
+                      });
+                    },
+                    icon: const Icon(Icons.arrow_back_ios_new,),
+                  ),
+              ],),
+            ),
+            Text("$reapt/32",textAlign:TextAlign.center ,style:const TextStyle(fontSize: 25, fontWeight: FontWeight.bold,) ,),
+            Text("Round $round",textAlign:TextAlign.center ,style:const TextStyle(fontSize: 25, fontWeight: FontWeight.bold,) ,),
+            OutlinedButton(
+              onPressed: (){
+                setState(() {
+                  
+                });
+              }, 
+              child:const Text("Reset"),
+            ),
+            GestureDetector(
+              onTap:(){
+                setState(() {
+                  
+                });
+              },
+              child:const CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 36, 185, 151),
+                child:Text("Sabe7",textAlign:TextAlign.center ,style:TextStyle(fontSize: 25, fontWeight: FontWeight.bold,) ,),
+              ) ,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
